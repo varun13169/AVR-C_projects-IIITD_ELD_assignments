0baebb2c29eb4680930199d1eaa1dccb8438b0fb
varun2013169 <varun13169@iiitd.ac.in>
2015-03-22 23:03:35 +0530

    Ehrr again!!

---
+++ Blinking LED-assignment 2/blinking_led.c
@@ -0,0 +1,17 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main (void)
+{
+    DDRB=0x01;
+
+    while(1)
+    {
+        PORTB=0xFF;
+        _delay_ms(1000);
+		 PORTB=0x00;
+        _delay_ms(1000);
+
+    }
+}
